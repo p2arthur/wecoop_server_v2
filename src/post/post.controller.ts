32c2bb6c107ac441c04540d05e2edd7b58ddbc38
p2arthur <arthurrabelopro@gmail.com>
2024-09-06 18:52:42 -0700

    [add:route] post/:transactionId to get a single post data

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PostService } from './post.service';
+import { json } from 'stream/consumers';
 
 @Controller('post')
 export class PostController {
   constructor(private postServices: PostService) {}
 
   @Get('/:postTransactionId')
-  getPostByTransactionId(
+  async getPostByTransactionId(
     @Param('postTransactionId') postTransactionId: string,
-  ) {}
+  ) {
+    const data = await this.postServices.getPostByTransactionId(
+      'L42BURVVB6Z6CVTHRAM4XBISMYHBSGACK6KELPVHNCX6HSYBVSDQ',
+    );
+
+    console.log('data', data);
+
+    return data;
+  }
 }
