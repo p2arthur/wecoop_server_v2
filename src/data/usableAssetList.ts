40e5ed6265811af9e5d3f1d0fb098ac5b8ffebf2
p2arthur <arthurrabelopro@gmail.com>
2024-09-15 14:15:59 -0700

    [refactor:api] refactor api to use usableAssets list instead of instantiating each asset

--- src/data/usableAssetList.ts
+++ src/data/usableAssetList.ts
@@ -1,7 +1,19 @@
 export const usableAssetsList = [
-  { name: 'Coop', assetId: 796425061, image: '' },
-  { name: 'Algo', assetId: 0, image: '' },
-  { name: 'xUSD', assetId: 760037151, image: '' },
+  {
+    name: 'Coop',
+    assetId: 796425061,
+    image: 'https://asa-list.tinyman.org/assets/796425061/icon.png',
+  },
+  {
+    name: 'Algo',
+    assetId: 0,
+    image: 'https://asa-list.tinyman.org/assets/0/icon.png',
+  },
+  {
+    name: 'xUSD',
+    assetId: 760037151,
+    image: 'https://asa-list.tinyman.org/assets/760037151/icon.png',
+  },
   {
     name: '$JAWS',
     assetId: 2155690250,
@@ -19,5 +31,22 @@ export const usableAssetsList = [
     assetId: 1265975021,
     image:
       'https://algorand-wallet-mainnet.b-cdn.net/media/asset_verification_requests_logo_png/2024/01/02/d9699190064a41679493a0b5c30d0d83.png?width=200&quality=70',
+  },
+  {
+    name: 'AKTA',
+    assetId: 523683256,
+    image: 'https://asa-list.tinyman.org/assets/523683256/icon.png',
+  },
+  {
+    name: 'A200',
+    assetId: 1682662165,
+    image:
+      'https://algorand-wallet-mainnet.b-cdn.net/media/asset_verification_requests_logo_png/2024/03/22/ff48d99492a840a7bd54dce69c7847ec.png?width=200&quality=70',
+  },
+  {
+    name: 'TINY',
+    assetId: 2200000000,
+    image:
+      'https://algorand-wallet-mainnet.b-cdn.net/media/asset_verification_requests_logo_png/2024/07/29/238ed21b2e2c4ce9a5a11a7cfe4c50aa.png?width=200&quality=70',
   },
 ];
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -3,22 +3,16 @@ import axios from 'axios';
 import * as base64 from 'base-64';
 import { WalletAddress } from 'src/enums/WalletAddress';
 import { NotePrefix } from 'src/enums/NotePrefix';
-import { AssetId } from 'src/enums/AssetId';
-import { Fees } from 'src/enums/Fee';
 import { PostInterface } from 'src/interfaces/PostInterface';
 import { PostService } from 'src/post/post.service';
 import { LikesService } from 'src/likes/likes.service';
 import { RepliesService } from 'src/replies/replies.service';
+import { usableAssetsList } from 'src/data/usableAssetList';
 
 @Injectable()
 export class FeedService {
-  //----------------------------------------------------------------------------
-  //Setting the prefix of this Class - Posts
   private notePrefix: string = NotePrefix.WeCoopPost;
-
-  //Setting the postsList when this Class is created and setting it to an empty array of Posts
   private postsList: PostInterface[] = [];
-  //----------------------------------------------------------------------------
 
   constructor(
     private postServices: PostService,
@@ -26,115 +20,75 @@ export class FeedService {
     private repliesServices: RepliesService,
   ) {}
 
-  // //Method to reset set indexer url based on a given address
-  // private setGetPostsUrl(address: string, assetId: number) {
-  //   return `https://mainnet-idx.algonode.cloud/v2/accounts/${address}/transactions?note-prefix=${base64.encode(
-  //     this.notePrefix,
-  //   )}&tx-type=axfer&asset-id=${assetId}&currency-greater-than=${
-  //     Fees.PostFee - 1
-  //   }&currency-less-than=${Fees.PostFee + 1}`;
-  // }
-  //Method to reset set indexer url based on a given address
-  private setGetPostsUrl(address: string, assetId: number) {
+  private setGetPostsUrl(address: string, assetId: number): string {
     return `https://mainnet-idx.algonode.cloud/v2/accounts/${address}/transactions?note-prefix=${base64.encode(
       this.notePrefix,
     )}&tx-type=axfer&asset-id=${assetId}`;
   }
 
-  //Method to reset postsList propertie of this class
-  private resetPostsList() {
+  private resetPostsList(): void {
     this.postsList = [];
   }
 
-  //Method to get all posts sent to the platform
-  //----------------------------------------------------------------------------
-  public async getAllPosts(walletAddres?: string) {
+  // Refactor to loop over usableAssetsList
+  public async getAllPosts(walletAddres?: string): Promise<PostInterface[]> {
     this.resetPostsList();
-    //TODO - Move likes logic to its own service - This is testing
+
     const likesUrl = `https://mainnet-idx.algonode.cloud/v2/accounts/DZ6ZKA6STPVTPCTGN2DO5J5NUYEETWOIB7XVPSJ4F3N2QZQTNS3Q7VIXCM/transactions?note-prefix=${btoa(
       NotePrefix.WeCoopLike,
     )}&tx-type=axfer`;
+
     const repliesUrl = `https://mainnet-idx.algonode.cloud/v2/accounts/DZ6ZKA6STPVTPCTGN2DO5J5NUYEETWOIB7XVPSJ4F3N2QZQTNS3Q7VIXCM/transactions?note-prefix=${btoa(
       NotePrefix.WeCoopReply,
     )}&tx-type=axfer`;
 
-    const { data: allLikes } = await axios.get(likesUrl);
-    const { data: allReplies } = await axios.get(repliesUrl);
+    // Fetch likes and replies
+    const [allLikes, allReplies] = await Promise.all([
+      axios.get(likesUrl).then((res) => res.data),
+      axios.get(repliesUrl).then((res) => res.data),
+    ]);
 
-    const coopPostsUrl = this.setGetPostsUrl(
-      WalletAddress.WeCoopMainAddress,
-      AssetId.coopCoin,
-    );
-    const xUsdPostsUrl = this.setGetPostsUrl(
-      WalletAddress.WeCoopMainAddress,
-      AssetId.xUSD,
-    );
-    const jawsPostsUrl = this.setGetPostsUrl(
-      WalletAddress.WeCoopMainAddress,
-      AssetId.jaws,
-    );
-    const orangePostsUrl = this.setGetPostsUrl(
-      WalletAddress.WeCoopMainAddress,
-      AssetId.orange,
-    );
-    const akitaPostsUrl = this.setGetPostsUrl(
-      WalletAddress.WeCoopMainAddress,
-      AssetId.akita,
-    );
+    // Loop through the usableAssetsList and get transactions for each asset
+    const allPostTransactions = (
+      await Promise.all(
+        usableAssetsList.map(async (usableAsset) => {
+          const assetPostsUrl = this.setGetPostsUrl(
+            WalletAddress.WeCoopMainAddress,
+            usableAsset.assetId,
+          );
+          const { data } = await axios.get(assetPostsUrl);
+          return data.transactions;
+        }),
+      )
+    ).flat();
 
-    const { data: coopPostsData } = await axios.get(coopPostsUrl);
-    const { transactions: coopPostsTransactions } = coopPostsData;
-
-    const { data: xUsdPostsData } = await axios.get(xUsdPostsUrl);
-    const { transactions: xUsdPostTransactions } = xUsdPostsData;
-
-    const { data: jawsPostsData } = await axios.get(jawsPostsUrl);
-    const { transactions: jawsPostTransactions } = jawsPostsData;
-
-    const { data: orangePostsData } = await axios.get(orangePostsUrl);
-    const { transactions: orangePostTransactions } = orangePostsData;
-
-    const { data: akitaPostsData } = await axios.get(akitaPostsUrl);
-    const { transactions: akitaPostTransactions } = akitaPostsData;
-
-    const allPostTransactions = [
-      ...coopPostsTransactions,
-      ...xUsdPostTransactions,
-      ...jawsPostTransactions,
-      ...orangePostTransactions,
-      ...akitaPostTransactions,
-    ];
-
+    // Sort transactions
     const sortedPostTransactions = allPostTransactions.sort(
       (a, b) => b['confirmed-round'] - a['confirmed-round'],
     );
 
-    let post: any = {};
+    // Process transactions into posts
+    for (const transaction of sortedPostTransactions) {
+      const post = await this.createPost(transaction, allLikes, allReplies);
+      if (post) this.postsList.push(post);
+    }
 
-    for (let transaction of sortedPostTransactions) {
-      // const post: PostInterface = await this.postServices.setPost(transaction);
+    return this.postsList;
+  }
 
+  private async createPost(
+    transaction: any,
+    allLikes: any,
+    allReplies: any,
+  ): Promise<PostInterface | null> {
+    try {
       const encodedNote = transaction.note;
       const decodedNote = atob(encodedNote);
-      const postData = decodedNote.split(':');
-      const postCountry = postData[2];
-      const postText = postData[3];
+      const [_, __, postCountry, postText] = decodedNote.split(':');
       const creatorAddress = transaction.sender;
       const transactionId = transaction.id;
       const timestamp = transaction['round-time'];
       const assetId = transaction['asset-transfer-transaction']['asset-id'];
-
-      post = {
-        text: postText,
-        creator_address: creatorAddress,
-        transaction_id: transactionId,
-        timestamp,
-        country: postCountry,
-        likes: 0,
-        replies: [],
-        status: 'accepted',
-        assetId,
-      };
 
       const postLikes = this.likesServices.filterLikesByPostTransactionId(
         transaction.id,
@@ -147,32 +101,21 @@ export class FeedService {
         allLikes,
       );
 
-      Object.assign(post, {
+      return {
+        text: postText,
+        creator_address: creatorAddress,
+        transaction_id: transactionId,
+        timestamp,
+        country: postCountry,
         likes: postLikes,
         replies: postReplies,
-      });
-
-      this.postsList.push(post);
+        status: 'accepted',
+        assetId,
+      };
+    } catch (error) {
+      console.error('Error creating post:', error);
+      return null;
     }
-
-    return this.postsList;
-  }
-  //----------------------------------------------------------------------------
-
-  //Method to get all posts created by a given address
-  //----------------------------------------------------------------------------
-  public async getAllPostsByAddress(walletAddress: string) {
-    const allPosts = await this.getAllPosts();
-
-    const uniquePostList = [];
-
-    allPosts.forEach((post) => {
-      if (post.creator_address === walletAddress) {
-        uniquePostList.push(post);
-      }
-    });
-    this.postsList = uniquePostList;
-    return this.postsList;
   }
 
   public async getFeedByWalletAddress(
@@ -259,6 +202,20 @@ export class FeedService {
       this.postsList.push(post);
     }
 
+    return this.postsList;
+  }
+
+  public async getAllPostsByAddress(walletAddress: string) {
+    const allPosts = await this.getAllPosts();
+
+    const uniquePostList = [];
+
+    allPosts.forEach((post) => {
+      if (post.creator_address === walletAddress) {
+        uniquePostList.push(post);
+      }
+    });
+    this.postsList = uniquePostList;
     return this.postsList;
   }
 }
