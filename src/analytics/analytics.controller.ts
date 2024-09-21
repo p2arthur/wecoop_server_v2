6df813f701502dedadf3a3f6a6d06c9fb7313d41
Diego Cardoso Marques <dmarques1508@gmail.com>
2024-09-21 02:22:19 -0300

    refactor: removendo comentários

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,28 +1,27 @@
-import { Controller, Get } from '@nestjs/common'; // Importa os decorators do NestJS
-import { AnalyticsService } from './analytics.service'; // Importa o serviço de analytics
+import { Controller, Get } from '@nestjs/common'; 
+import { AnalyticsService } from './analytics.service';
 
-@Controller('/analytics') // Define o caminho base para o controller
+@Controller('/analytics') 
 export class AnalyticsController {
-  constructor(private analyticsServices: AnalyticsService) {} // Injeta o serviço de analytics
+  constructor(private analyticsServices: AnalyticsService) {} 
 
-  // Rota para obter as carteiras com mais transações de likes
   @Get('/wallets/top-liked-wallets')
   async getTopLikedWallets() {
-    const topWallets = await this.analyticsServices.getTop10WalletsWithMostLikes(); // Busca as carteiras com mais transações de likes
-    return topWallets; // Retorna a lista das top 10 carteiras
+    const topWallets = await this.analyticsServices.getTop10WalletsWithMostLikes();
+    return topWallets; 
   }
 
   // Rota para obter os posts mais curtidos
   @Get('/posts/top-liked-posts')
   async getTopPostsIds() {
-    const topPosts = await this.analyticsServices.getTopLikedPostsIds(); // Busca os posts mais curtidos
-    return topPosts; // Retorna a lista dos top 10 posts mais curtidos
+    const topPosts = await this.analyticsServices.getTopLikedPostsIds(); 
+    return topPosts;
   }
 
   // Rota para obter os criadores com mais curtidas
   @Get('/creators/top-liked-creators')
   async getTopLikedCreatorsIds() {
-    const topCreators = await this.analyticsServices.getTopLikedCreatorsIds(); // Busca os criadores mais curtidos
-    return topCreators; // Retorna a lista dos top 10 criadores mais curtidos
+    const topCreators = await this.analyticsServices.getTopLikedCreatorsIds(); 
+    return topCreators; 
   }
 }
