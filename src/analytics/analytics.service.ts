62f648fefd37408ef7bfb3c26f5bb8ec4163997b
Diego Cardoso Marques <dmarques1508@gmail.com>
2024-09-21 02:10:21 -0300

    feat: getAllLikeTransactions

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,19 +1,82 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios'; 
+import { NotePrefix } from 'src/enums/NotePrefix'; 
 import { PostService } from 'src/post/post.service';
 import { LikesService } from 'src/likes/likes.service';
 
-@Injectable()
+@Injectable() 
 export class AnalyticsService {
   constructor(
     private postServices: PostService,
     private likesServices: LikesService,
   ) {}
 
+  public async getAllLikeTransactions() {
+    const likesUrl = `https://mainnet-idx.algonode.cloud/v2/accounts/DZ6ZKA6STPVTPCTGN2DO5J5NUYEETWOIB7XVPSJ4F3N2QZQTNS3Q7VIXCM/transactions?note-prefix=${btoa(
+      NotePrefix.WeCoopLike,
+    )}&tx-type=axfer`;
+
+    try {
+      const { data } = await axios.get(likesUrl);
+
+      const { transactions: likeTransactions } = data;
+
+
+      const allLikes = likeTransactions.map((transaction) => {
+        const encodedNote = transaction.note; 
+        const decodedNote = atob(encodedNote); 
+
+        const postId = decodedNote.split(':')[3]; 
+        const sender = transaction.sender; 
+
+        return { postId, sender }; 
+      });
+
+      return allLikes; 
+    } catch (error) {
+     
+      console.error('Erro ao buscar transações de likes:', error);
+      return [];
+    }
+  }
+
+ 
+  public async getTop10WalletsWithMostLikes() {
+    const allLikeTransactions = await this.getAllLikeTransactions();
+
+    const likesPerWallet: { [walletAddress: string]: number } = {}; 
+
+   
+    allLikeTransactions.forEach((transaction) => {
+      const walletAddress = transaction.sender; 
+
+
+      if (!likesPerWallet[walletAddress]) {
+        likesPerWallet[walletAddress] = 0;
+      }
+
+
+      likesPerWallet[walletAddress]++;
+    });
+
+
+    const sortedTop10Wallets = Object.keys(likesPerWallet)
+      .map((walletAddress) => ({
+        walletAddress, 
+        transactionCount: likesPerWallet[walletAddress], 
+      }))
+      .sort((a, b) => b.transactionCount - a.transactionCount)
+      .slice(0, 10); 
+
+    return sortedTop10Wallets; 
+  }
+
+
   public async getTopLikedPostsIds() {
     const allLikes = await this.likesServices.getAllLikes();
     const perPostLikesCounter: { [postId: string]: number } = {};
 
-    // Count likes per post
+
     allLikes.forEach((like) => {
       const postId = like.postId;
 
@@ -24,34 +87,36 @@ export class AnalyticsService {
       perPostLikesCounter[postId]++;
     });
 
-    // Convert the perPostLikesCounter object to an array of objects for sorting
+
     const sortedTop10LikedPosts = Object.keys(perPostLikesCounter)
       .map((postId) => ({
         postId,
-        likesCount: perPostLikesCounter[postId], // Ensure likesCount is included
+        likesCount: perPostLikesCounter[postId], 
       }))
-      .sort((a, b) => b.likesCount - a.likesCount) // Sort by likesCount in descending order
-      .slice(0, 10); // Return the top 10 posts
+      .sort((a, b) => b.likesCount - a.likesCount) 
+      .slice(0, 10);
 
-    return sortedTop10LikedPosts; // This should now correctly return the postId and likesCount
+    return sortedTop10LikedPosts; 
   }
 
+
+
   async getTopLikedCreatorsIds() {
-    const top10LikedPosts = await this.getTopLikedPostsIds(); // Get the top liked posts
+    const top10LikedPosts = await this.getTopLikedPostsIds(); 
     const top10LikedCreators: { [creatorId: string]: number } = {};
 
-    // Process each liked post to accumulate likes per creator
+
     await Promise.all(
       top10LikedPosts.map(async (likedPost) => {
         const postId = likedPost.postId;
         const likesCount = likedPost.likesCount;
 
-        // Fetch the creator using postId
+
         const creator =
           await this.postServices.getPostCreatorByTransactionId(postId);
 
         if (creator) {
-          // Accumulate likes per creator
+
           if (!top10LikedCreators[creator]) {
             top10LikedCreators[creator] = likesCount;
           } else {
@@ -61,14 +126,14 @@ export class AnalyticsService {
       }),
     );
 
-    // Convert the top10LikedCreators object to an array of objects for sorting
+
     const sortedTop10LikedCreators = Object.keys(top10LikedCreators)
       .map((creatorAddress) => ({
         creatorAddress,
-        likesCount: top10LikedCreators[creatorAddress], // Include the likesCount for each creator
+        likesCount: top10LikedCreators[creatorAddress], 
       }))
-      .sort((a, b) => b.likesCount - a.likesCount); // Sort in descending order
+      .sort((a, b) => b.likesCount - a.likesCount); 
 
-    return sortedTop10LikedCreators; // Return sorted creators with likesCount
+    return sortedTop10LikedCreators;
   }
 }
