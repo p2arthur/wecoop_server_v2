55224ec708c14ea1fd5b4911a794e8f8d3748eb5
Diego Cardoso Marques <dmarques1508@gmail.com>
2024-09-21 19:02:47 -0300

    feat: create leaderboard and counter all interactions

---
+++ src/leaderboard/leaderboard.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { LeaderboardService } from './leaderboard.service';
+
+@Controller('leaderboard')
+export class LeaderboardController {
+  constructor(private leaderboardService: LeaderboardService) {}
+
+  @Get('top-creators')
+  public async getTopCreators() {
+    const topCreators = await this.leaderboardService.getTopCreators();
+    return topCreators;
+  }
+}
