6bb6d5897c05342d062698659424775450bd1d16
FelipeQueiroz <felipe.queiroz@lekto.com.br>
2024-10-23 01:36:41 -0300

    feat: adding discord bot for polls

---
+++ src/utils/postOnDiscord.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import * as fs from 'fs';
+import FormData = require('form-data'); // Importação correta do FormData
+
+export async function sendPollToDiscord(text: string, localImagePath: string) {
+  const webhookUrl = "https://discord.com/api/webhooks/1298479608953438302/Yu4-LUQvajB8Ivo5GOaJr6TdAyWAWwEJw-yeoNs0f293wyxIGCRr_XXycwsl2Pcgnz2i";
+
+  // Certifique-se de que o caminho da imagem é válido e o arquivo existe
+  if (!fs.existsSync(localImagePath)) {
+    console.error(`File not found: ${localImagePath}`);
+    return;
+  }
+
+  const form = new FormData();
+  form.append('content', text);
+  form.append('file', fs.createReadStream(localImagePath), 'poll.png');
+
+  try {
+    await axios.post(webhookUrl, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
+    console.log(' ----------------- Poll sent to Discord ----------------- ')
+  } catch (error) {
+    console.error('Error sending message to Discord:', error);
+  }
+}
